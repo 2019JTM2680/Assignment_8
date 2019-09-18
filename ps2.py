4a06def1990ccec4871bf3b52a77f5ecabfd544e
2019JTM2680 <jtm192680@iitd.ac.in>
2019-09-18 10:58:44 +0530

    Start of second program. I did few lines for changing color and alignments.

--- ps2.py
+++ ps2.py
@@ -1,1 +1,8 @@
-
+#!/usr/bin/python3
+#The below functios are declared to format the text(like adding colors and alignment)
+import shutil
+columns = shutil.get_terminal_size().columns
+def prRed(skk): print("\033[91m {}\033[00m" .format(skk)) 
+def prGreen(skk): print("\033[92m {}\033[00m" .format(skk)) 
+#main program starts here
+prRed("TIC-TAC-TOE".center(columns))
