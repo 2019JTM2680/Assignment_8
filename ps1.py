d6d003b13f7b21db5ca8f2475a8deabdce0ada3a
2019JTM2680 <jtm192680@iitd.ac.in>
2019-09-18 09:28:43 +0530

    Started the program. I did till counting number of ones for parity check

--- ps1.py
+++ ps1.py
@@ -1,1 +1,13 @@
-
+#!/usr/bin/python
+#Reading the number
+bin = int(input("Enter the binary number to br encoded:	"))
+print (bin)
+#Splitting the number into digits and storing it to a list
+digits = [int(d) for d in str(bin)]
+print (digits)
+count=0
+#counting the number of ones to implement parity check
+for i in digits:
+	if i==1:
+		count+=1
+print (count) 
